f1ca06b8d519e866106a96ef2f7dcd2baee5f5c6
Hendra Dwi Saputra <hendra@usa.com>
2025-06-16 20:22:34 +0700

    first commit

---
+++ window.py
@@ -0,0 +1,61 @@
+from PySide6.QtWidgets import (
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QListWidget,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("GAMBAR MEDIS | GADIS")
+        self.resize(800, 564)
+
+        self.central = QWidget()
+        self.setCentralWidget(self.central)
+
+        self.main_horizontal = QHBoxLayout()
+        self.central.setLayout(self.main_horizontal)
+
+        self.left_layout = QVBoxLayout()
+
+        self.dcmViewer = QOpenGLWidget()
+        self.dcmViewer.setMinimumHeight(471)
+        self.left_layout.addWidget(self.dcmViewer)
+
+        self.bottom_layout = QHBoxLayout()
+
+        self.filePath = QLineEdit()
+        self.filePath.setPlaceholderText("Dicom File...")
+        self.filePath.setReadOnly(True)
+
+        self.processButton = QPushButton("Process")
+        self.processButton.setMinimumWidth(120)
+
+        self.bottom_layout.addWidget(self.filePath)
+        self.bottom_layout.addWidget(self.processButton)
+
+        self.left_layout.addLayout(self.bottom_layout)
+
+        self.main_horizontal.addLayout(self.left_layout, stretch=2)
+
+        self.right_layout = QVBoxLayout()
+
+        self.files_label = QLabel("DICOM FILE LIST")
+        self.right_layout.addWidget(self.files_label)
+
+        self.fileList = QListWidget()
+        self.fileList.setMinimumWidth(100)
+        self.right_layout.addWidget(self.fileList)
+
+        self.main_horizontal.addLayout(self.right_layout, stretch=1)
+
+        self.processButton.clicked.connect(self.process_file)
+
+    def process_file(self):
+        print("Proses file DICOM:", self.filePath.text())
